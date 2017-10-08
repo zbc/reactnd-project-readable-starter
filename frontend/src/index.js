72e68387b8c2d9a7b187034ae778eb049feb3a5e
Bocong Zhao <zhaobocong@hotmail.com>
2017-10-08 17:37:08 -0500

    Initial Project

---
+++ frontend/src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+import promise from 'redux-promise';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route, Switch} from 'react-router-dom';
+
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
+ReactDOM.render(
+    <Provider store={createStoreWithMiddleware(reducers)}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
+    , document.getElementById('root'));
+registerServiceWorker();
